4a22517381505ddc59960886db41dc3af409870a
fallenidol <paul@mcilreavy.com>
2015-06-16 16:48:39 +1000

    refactor

---
+++ wwwroot/js/_viewModel.js
@@ -0,0 +1,33 @@
+﻿
+var viewModel = function () {
+    var self = this;
+
+
+    self.rowClicked = function (tr, data) {
+        console.log(data);
+    };
+
+    self.toggleRowClickability = function (evt) {
+        self.rowsClickable($(this).prop('checked') === true)
+    };
+
+    self.loadAllData = function getData() {
+        $.ajax({
+            url: 'api/Data',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+
+                self.setItems(data);
+            },
+            error: function (x, y, z) {
+                console.log([x, y, z]);
+            }
+        });
+    };
+
+
+    self.loadAllData();
+
+};
